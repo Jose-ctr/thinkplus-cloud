94098bf3b76838af27d75d8047cd688a1613c7fc
Joseph | Founder ThinkPlus <146562490+Jose-ctr@users.noreply.github.com>
2026-08-06 22:59:26 +0300

    Update database connection settings

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-$host = "localhost";
-$user = "root"; 
-$pass = ""; 
-$db = "thinkplus_mvp";
+$host = "sql303.infinityfree.com";
+$user = "if0_42459348"; 
+$pass = "ThinkPlus2026"; 
+$db = "if0_42459348_thinkplus";
 
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
